07a7fe91c3fe635e3555289e5dd1b9958822c6d4
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-10-25 17:40:15 +0200

    Update EMbC.r

--- EMbC.r
+++ EMbC.r
@@ -1,7 +1,5 @@
 library(EMbC)
-library(tools)
 library(dplyr)
-library(Rcpp)
 
 
 wd <- "C:\\Users\\micci\\Desktop\\Monitoring seabirds\\Monitoring-seabirds-through-satellite-images\\Data"
@@ -11,6 +9,7 @@ df <- read.csv("FileBerteAnalisiIndividuale.csv")
 head(df)
 df2 <- df %>% filter(device_id == 201564) %>% select("UTC_datetime", "Longitude", "Latitude")
 head(df2)
-#data_matrix <- data.matrix(df2)
-#library(Rcpp)
+
 shwbc <- stbc(df2)
+
+df2$BA <-  shwbc@A
