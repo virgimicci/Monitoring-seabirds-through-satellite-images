5d043990d03b71b42868eb990eb94773079d57f2
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-10-25 17:38:15 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,21 +1,6 @@
 
-import math
+
 import pandas as pd
-
-# Function to calculate distance among two points
-def distance(origin, destination):
-    lat1, lon1 = origin
-    lat2, lon2 = destination
-    radius = 6371 # km
-
-    dlat = math.radians(lat2-lat1)
-    dlon = math.radians(lon2-lon1)
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
-        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = radius * c
-
-    return d
 
 # Function to classify birds activity 
 # where:
@@ -35,7 +20,3 @@ def classification(row):
             return "T"
 
 
-
-
-
-    
