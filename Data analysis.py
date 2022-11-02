f71f46760f5dd42cafea9ac1e03ab56b40ff5753
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-11-02 10:05:39 +0100

    Update Data analysis.py

--- Data analysis.py
+++ Data analysis.py
@@ -28,28 +28,29 @@ df["interval"]=df.groupby("device_id").time.diff().dt.seconds.div(60)
 df["interval"].mean() 
 
 # Drop fix with an interval > 10 min 
-df.drop(df[df['interval'] >= 11].index, inplace = True)
+df.drop(df[df['interval'] >= 12].index, inplace = True)
 
 # Creating new col with shifted values of lat and long to calculate distances
 df[['Latitude2', 'Longitude2']] = df[['Latitude', 'Longitude']].shift(-1)
 df[['Latitude3', 'Longitude3']] = df[['Latitude', 'Longitude']].shift(-2)
 
-# Calculate distances from a fix and following one
+# Calculate distances from a fix and following one with the haversine function
 # I would use geopy function to calculate the distance because is more accurate
 # but it gives me problem while I try to sum the values for the df["arco"], REMEMBER TO TRY TO FIX THE PROB 
 df["geo_dist"] = df.apply(lambda x: haversine((x['Latitude'], x['Longitude']),(x['Latitude2'], x['Longitude2'])) if x['Latitude2'] > 0 or x['Longitude2'] > 0 else 0, axis =1)
 
-# let's calculate the arc for each point which is the distance among three consecutive points 
+# Let's calculate the arc for each point which is the distance among three consecutive points 
 df["arc"] = df.groupby("device_id")["geo_dist"].apply(lambda x: x + x.shift(-1))
 # Let's calculate the chord which is the distance between a point and the second consecutive 
 df["chord"] = df.apply(lambda x: haversine((x['Latitude'], x['Longitude']),(x['Latitude3'], x['Longitude3'])) if x['Latitude3'] > 0 or x['Longitude3'] > 0 else 0, axis =1)
 
 # I have to transform 0 value in NaN oterwise I cannot divide because I have 0 values in df["arco"]
 df["arc"] = df["arc"].replace({ 0:np.nan})
+# Let's calculate the tortuosity index
 df["tortuosity index"] = df["chord"]/df["arc"] 
 
 
-# let's do the median in each row of the df["Speed_m_s"] in m/s with the following 4 values
+# Let's do the median in each row of the df["Speed_m_s"] in m/s with the following 4 values
 # probably I have a multiindex problem so i have to apply reset_index()
 med = df.groupby("device_id")["Speed_m_s"].rolling(5).median().shift(-4)
 df["median"] = med.reset_index(level=0, drop=True)
@@ -67,7 +68,7 @@ df['birds_activity'] = df.apply(classification, axis=1)
 
 sum(df['birds_activity'] == "F")
 
-# I made a gpd with the col that i needed 
+# I made a df with the col that i needed 
 df_c = df[["device_id", "timestamp", "Longitude", "Latitude", 
     "Speed_m_s", "geo_dist", "arc", "chord",
     "tortuosity index", "birds_activity"]].copy()
